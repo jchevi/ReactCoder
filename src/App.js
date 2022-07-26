a5fffa89af07a81e49be02b184be04eb2bf90062
Juan Marmo <juanichevillard@gmail.com>
2022-07-26 21:45:59 +0200

    primer commit al repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from "NavBar"
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+      <main>
+        <h2>Home</h2>
+      </main>
+      <footer>
+        <p>footer</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+      <header id="Nav">
+        <h1>MercadoSneakers</h1>
+        <a href="#">link 1</a>
+        <a href="#">link 2</a>
+      </header>
+  );
+}
+
+export default NavBar;
